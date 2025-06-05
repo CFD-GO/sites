7eec313ba08cb6fe55445e7ff84faa23c03f1dff
Lukasz Laniewski-Wollk <lukasz.laniewski@pw.edu.pl>
2025-06-05 16:04:41 +0200

    Update the script for downloading and converting first pages

--- auto/download.pdf.R
+++ auto/download.pdf.R
@@ -1,5 +1,5 @@
 
-ft_links = work_tab$ft_url
+ft_links = works_tab$ft_url
 sel = !is.na(ft_links) & !is.na(works_tab$doi)
 for (i in which(sel)) {
   print(works_tab$doi[i])
@@ -11,3 +11,19 @@ for (i in which(sel)) {
     try(curl::curl_download(scopus_url(url),fn))
   } else cat("exists\n")
 }
+
+fp = data.frame(pdf = dir("~/doi",recursive=TRUE,pattern="*.pdf"))
+fp$doi = sub(".pdf$","",fp$pdf)
+fp$pdf = paste0(Sys.getenv("HOME"),"/doi/",fp$doi,".pdf")
+fp$png = paste0("static/img/doi/",fp$doi,"/firstpage.png")
+
+sel = file.exists(fp$pdf) & (!file.exists(fp$png))
+for (i in which(sel)) {
+  dir.create(dirname(fp$png[i]),recursive = TRUE,showWarnings = FALSE)
+  command = paste0("convert -density 150 -background white -alpha remove '",fp$pdf[i], "'[0] '",fp$png[i],"'")
+  #command = paste0("gs -sDEVICE=png16m -r300 -dDownScaleFactor=4 -sOutputFile=",fp$png[i]," -dLastPage=1 ",fp$pdf[i])
+  print(command)
+  system(command=command)
+}
+
+
